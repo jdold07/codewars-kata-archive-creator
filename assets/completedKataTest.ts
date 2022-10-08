275cdfaf9d6c3ec7bf070f39be15fb76912c6578
jdold07 <93500023+jdold07@users.noreply.github.com>
2022-10-08 10:42:53 +1000

    Template test data only

---
+++ assets/completedKataTest.ts
@@ -0,0 +1,77 @@
+// export
+const completedKataTests = {
+  totalPages: 4,
+  totalItems: 773,
+  kata: [
+    {
+      id: "5324945e2ece5e1f32000370",
+      name: "Sum Strings as Numbers",
+      slug: "sum-strings-as-numbers",
+      completedLanguages: ["javascript"],
+      completedAt: "2022-09-29T04:21:06.740Z"
+    },
+    {
+      id: "5b096efeaf15bef812000010",
+      name: "nth Floyd line",
+      slug: "nth-floyd-line",
+      completedLanguages: ["python", "javascript"],
+      completedAt: "2022-09-27T13:44:22.843Z"
+    },
+    {
+      id: "5ef9ca8b76be6d001d5e1c3e",
+      name: "Error correction #1 - Hamming Code",
+      slug: "error-correction-number-1-hamming-code",
+      completedLanguages: ["javascript", "typescript", "coffeescript", "python"],
+      completedAt: "2022-09-27T08:57:56.239Z"
+    },
+    {
+      id: "579ba41ce298a73aaa000255",
+      name: "Name That Number!",
+      slug: "name-that-number",
+      completedLanguages: ["javascript", "python"],
+      completedAt: "2022-09-27T07:34:47.315Z"
+    },
+    {
+      id: "57adadd36b34faea6b00031b",
+      name: "Vault experience (3): Populate the vaults",
+      slug: "vault-experience-3-populate-the-vaults",
+      completedLanguages: ["python"],
+      completedAt: "2022-09-27T06:03:13.864Z"
+    },
+    {
+      id: "55b350026cc02ac1a7000032",
+      name: "Convert integer to Whitespace format",
+      slug: "convert-integer-to-whitespace-format",
+      completedLanguages: ["javascript", "coffeescript", "python"],
+      completedAt: "2022-09-26T23:08:01.148Z"
+    },
+    {
+      id: "59aac10dd0a5ff951100002a",
+      name: "Sort array by sorting its smallest sub-array",
+      slug: "sort-array-by-sorting-its-smallest-sub-array",
+      completedLanguages: ["javascript"],
+      completedAt: "2022-09-26T02:07:43.272Z"
+    },
+    {
+      id: "5282b48bb70058e4c4000fa7",
+      name: "Convert A Hex String To RGB",
+      slug: "convert-a-hex-string-to-rgb",
+      completedLanguages: ["javascript", "coffeescript", "python", "swift"],
+      completedAt: "2022-09-25T08:39:30.349Z"
+    },
+    {
+      id: "5613d06cee1e7da6d5000055",
+      name: "Steps in Primes",
+      slug: "steps-in-primes",
+      completedLanguages: ["typescript", "javascript", "coffeescript", "python"],
+      completedAt: "2022-09-24T13:09:08.727Z"
+    },
+    {
+      id: "5583d268479559400d000064",
+      name: "Binary to Text (ASCII) Conversion",
+      slug: "binary-to-text-ascii-conversion",
+      completedLanguages: ["javascript", "typescript", "python", "coffeescript"],
+      completedAt: "2022-09-24T11:12:25.487Z"
+    }
+  ]
+}
