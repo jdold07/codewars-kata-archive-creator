6488d88ddea5f594b487345237640bcc3ee8e848
jdold07 <93500023+jdold07@users.noreply.github.com>
2022-10-14 13:49:49 +1000

    Loop relocation for updates > 1

--- src/components/getKataDetails.ts
+++ src/components/getKataDetails.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 import path from "path"
 import { rootPath } from "../../private/config/config"
 
-export default async function getKataDetails(filteredCompletedKatas: Promise<any>): Promise<any> {
+export default async function getKataDetails(kata: any): Promise<any> {
   /** Fetch completed kata detail from Codewars API & process folders & markdown description file
    * @Param const filteredCompletedKataList:
    *    data: {
@@ -14,20 +14,17 @@ export default async function getKataDetails(filteredCompletedKatas: Promise<any
    *      completedAt: string
    *    }[]
    */
-  const katas = await filteredCompletedKatas
-  for (const kata of katas) {
-    try {
-      const response = await axios.get(`https://www.codewars.com/api/v1/code-challenges/${kata.id}`)
+  try {
+    const response = await axios.get(`https://www.codewars.com/api/v1/code-challenges/${kata.id}`)
 
-      // Add an underscore to the beginning of any slug that starts with a number for path compatibility
-      response.data.slug = await response.data.slug.replace(/^(\d)/, "_$1")
-      const kataRankDirName = `kata-${Math.abs(response.data.rank.id) || "beta"}-kyu`
-      const kataPath: string = path.join(rootPath, kataRankDirName, response.data.slug)
+    // Add an underscore to the beginning of any slug that starts with a number for path compatibility
+    response.data.slug = await response.data.slug.replace(/^(\d)/, "_$1")
+    const kataRankDirName = `kata-${Math.abs(response.data.rank.id) || "beta"}-kyu`
+    const kataPath: string = path.join(rootPath, kataRankDirName, response.data.slug)
 
-      return await Object.assign({}, kata, response.data, { kataRankDirName: kataRankDirName, kataPath: kataPath })
-    } catch (error) {
-      console.error(`Error from getKataDetails() in ${path.basename(__filename)}`)
-      throw Error(`Error collating kata detail for ${kata.slug}\n${error}`)
-    }
+    return await Object.assign({}, kata, response.data, { kataRankDirName: kataRankDirName, kataPath: kataPath })
+  } catch (error) {
+    console.error(`Error from getKataDetails() in ${path.basename(__filename)}`)
+    throw Error(`Error collating kata detail for ${kata.slug}\n${error}`)
   }
 }
