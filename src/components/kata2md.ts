fa6098f78f20b65fea8320282a64f5726a32fd8e
jdold07 <tim@tpstech.dev>
2023-03-06 22:39:52 +1000

    refactor: ♻️ cleanup & await runCodeWrites

--- src/components/kata2md.ts
+++ src/components/kata2md.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import getUserCompletedList from "./getUserCompletedList"
 import getKataDetails from "./getKataDetails"
-import * as Writes from "./writeToFile"
-import parseForMD from "./parseForMD"
+import getUserCompletedList from "./getUserCompletedList"
 import getUserSolutionsList from "./getUserSolutionsList"
 import { combineData, runCodeWrites } from "./helpers"
+import parseForMD from "./parseForMD"
 import processCodeStrings from "./processCodeStrings"
+import * as Writes from "./writeToFile"
 
 /**
  * Main kata2markdown App
@@ -13,12 +13,12 @@ import processCodeStrings from "./processCodeStrings"
 export default async () => {
   const filteredUserCompletedList = await getUserCompletedList()
   const userSolutionsList = await getUserSolutionsList()
-  for await (const kata of filteredUserCompletedList) {
+  for (const kata of filteredUserCompletedList) {
     const kataDetailWithRankPath = await getKataDetails(kata)
     //TODO Make the kata root directory creation & markdown write only run once per Kata ID.
     //TODO At the moment this runs unnecessarily for every language a Kata has been completed in.
     await Writes.createKataRootDir(kataDetailWithRankPath)
-    const mdString = parseForMD(kataDetailWithRankPath)
+    const mdString = await parseForMD(kataDetailWithRankPath)
     await Writes.writeKataMarkdownFile(kataDetailWithRankPath, mdString)
     for await (const language of kataDetailWithRankPath.completedLanguages) {
       const combinedKataData = await combineData(
